d02a6151f78ad478308dd9f1580bd771927b3861
Jon Gilman <jon.gilman@gmail.com>
2013-07-07 12:53:05 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Brunchinbrookline::Application.config.secret_token = 'c503a05d687f3a640a395ec62df2fbe86993b99264ea37d4996750e8346cd24ab6b5046ad0b82b44ea65ecfd65f618381e072b2cc8bc2e8d929f415526d6f4fd'
